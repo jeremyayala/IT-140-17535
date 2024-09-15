160de7f6619c50589f490656136e2dd1779f2f25
jeremyayala <jeremyayala@gmail.com>
2024-09-15 16:22:17 -0500

    making more robust

--- Module 2/NameAgeJeremyAyala.py
+++ Module 2/NameAgeJeremyAyala.py
@@ -1,16 +1,18 @@
 # This is a program that will ask for the users name and age and output their date of birth
 # Take user's name as in input and store into userName variable
 
-userName = input("What is your name?: ")
+from datetime import date
+
+userName = input("What is your name?\n")
 
 # take user's year of birth as input and store into userAge variable
 
-userAge = input("How old are you?:")
+userAge = int(input("How old are you?\n"))
 
 # Logic to subtract userAge from current year to get year of birth
 
-userYob = int(2024) - int(userAge)
+userYob = date.today().year - userAge
 
 # Printing the output to the command line
 
-print('Hello', userName, '! You were born in ', userYob, '.')
+print('Hello' + ' ' + userName + '! You were born in ', userYob, '.\n')
