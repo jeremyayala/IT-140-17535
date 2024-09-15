f0eafd9fc16087c2fcf519cb92a14e9f7c585b38
jeremyayala <jeremyayala@gmail.com>
2024-09-15 16:12:09 -0500

    Changing comment names

--- Module 2/NameAgeJeremyAyala.py
+++ Module 2/NameAgeJeremyAyala.py
@@ -1,17 +1,16 @@
-# This is a program that will ask for the users name and age
-
-# Take user's name as in input and store into user_name variable
+# This is a program that will ask for the users name and age and output their date of birth
+# Take user's name as in input and store into userName variable
 
 userName = input("What is your name?: ")
 
-# take user's year of birth as inout
+# take user's year of birth as input and store into userAge variable
 
 userAge = input("How old are you?:")
 
-# Logic to subtract user age from current year to get yeaar of birth
+# Logic to subtract userAge from current year to get year of birth
 
 userYob = int(2024) - int(userAge)
 
-# Returning the input
+# Printing the output to the command line
 
 print('Hello', userName, '! You were born in ', userYob, '.')
